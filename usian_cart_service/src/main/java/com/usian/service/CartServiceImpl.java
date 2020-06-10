548beae128eaf25158c5d1489b075f224dcca939
Administrator <1651419519@qq.com>
2020-06-10 20:07:35 +0800

    发布工程到github

---
+++ usian_cart_service/src/main/java/com/usian/service/CartServiceImpl.java
@@ -0,0 +1,31 @@
+package com.usian.service;
+
+import com.usian.pojo.TbItem;
+import com.usian.redis.RedisClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+@Service
+@Transactional
+public class CartServiceImpl implements CartService {
+
+    @Value("${CART_REDIS_KEY}")
+    private String CART_REDIS_KEY;
+
+    @Autowired
+    private RedisClient redisClient;
+
+    @Override
+    public Map<String, TbItem> selectCartByUserId(String userId) {
+        return (Map<String, TbItem>) redisClient.hget(CART_REDIS_KEY,userId);
+    }
+
+    @Override
+    public Boolean insertCart(String userId, Map<String, TbItem> cart) {
+        return redisClient.hset(CART_REDIS_KEY,userId,cart);
+    }
+}
